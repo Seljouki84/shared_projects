1b3d014605f30e24e8c001a3ba6cf997d69a4308
Seljouki84 <133975636+Seljouki84@users.noreply.github.com>
2023-06-05 21:23:13 +0100

    Add files via upload

---
+++ convert sting to datetime.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from datetime import datetime
+kpi_workbook = pd.read_excel(r'D:\Maintenance\Bloc 6\PROJECT KPI\test.xlsx', sheet_name="Sheet1", engine='openpyxl')
+
+for cell in kpi_workbook["date"]:
+    data_type = type(cell)
+    if data_type == str:
+       date_time_object= datetime.strptime(cell , '%d/%m/%Y %H:%M:%S')
+       print(date_time_object)
+    else:
+        print(cell)
+  
